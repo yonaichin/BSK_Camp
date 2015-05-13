26e2cf0c69df09f1e4ba5ca7265bbffe620b5ea8
Ponpon <sheep0406@hotmail.com>
2015-05-14 01:25:02 +0800

    Add navbar footer & some practice for coach

---
+++ app/controllers/basketball_controller.rb
@@ -0,0 +1,11 @@
+class BasketballController < ApplicationController
+	def index
+	end
+
+	def coach
+	end
+	
+	def course
+	end	
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root :to => "welcome#index"
   get "welcome/" => "welcome#index"
+
+  get "basketball/" => "basketball#index"
+  get "basketball/coach" => "basketball#coach"
+  get "basketball/course" => "basketball#course"
 end
